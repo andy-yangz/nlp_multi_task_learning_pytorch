99a75f679b8fdcc3ca271f8d13a8cbf30252c54f
sean.narenthiran <sean.narenthiran@digitalreasoning.com>
2019-01-03 12:41:32 +0000

    Cleaned show results script

--- show_result.py
+++ show_result.py
@@ -8,13 +8,14 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--input_path', default='result/')
 args = parser.parse_args()
 file_paths = [y for x in os.walk(args.input_path) for y in glob(os.path.join(x[0], '*.pt'))]
-results = []
+headers = ['POS', 'Chunking', 'NER']
 for file_path in file_paths:
     if 'result.pt' in file_path:
         name = file_path.split('/')[-1].split('.')[0]
-        best_accuracy = torch.load(file_path)['test_accuracies']
-        for val in best_accuracy:
-            if type(val) is tuple or type(val) is list:
-                print(name, ['%0.2f%%' % (float(x) * 100) for x in val])
+        test_accuracies = torch.load(file_path)['test_accuracies']
+        for test_results in test_accuracies:
+            if type(test_results) is tuple or type(test_results) is list:
+                results = zip(headers[:len(test_results)], ['%0.2f%%' % (float(x) * 100) for x in test_results])
             else:
-                print(name, ['%0.2f%%' % (float(val) * 100)])
+                results = [(headers[0], ': %0.2f%%' % (float(test_results) * 100))]
+            print(name, dict(results))
